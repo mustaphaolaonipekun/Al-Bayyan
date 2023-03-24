2754db53008bdd31340aeadd6a8582ccc7b2a4ba
Musty$dev <90248827+mustaphaolaonipekun@users.noreply.github.com>
2023-03-24 13:12:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -61,6 +61,6 @@ function showSlides() {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 
-  setTimeout(showSlides, 8000);
+  setTimeout(showSlides, 15000);
 }
 
